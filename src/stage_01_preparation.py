b43d0d4ac64ac352457da3343168fbc0e60c4c4f
Karthiksaran-001 <mailmekarthik001@gmail.com>
2022-10-28 18:22:26 +0530

    split train and test

--- src/stage_01_preparation.py
+++ src/stage_01_preparation.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_mng import process_posts
 import random
 
 STAGE = "One" ## <<< change stage name 
@@ -37,8 +38,7 @@ def load_data(config_path, params_path):
     with open(input_data , encoding=encode) as fd_in:
         with open(train_data_path , "w" , encoding=encode) as fd_out_train:
             with open(test_data_path , "w" , encoding=encode) as fd_out_test:
-                pass
-                # process_posts(fd_in , fd_out_train , fd_out_test , "<python>" , split)
+                process_posts(fd_in , fd_out_train , fd_out_test , "<python>" , split)
 
 
 
